4ac55f2a23d0d1b804629fb874dcec2eeffa4f19
Rizwan4141 <105095961+Rizwan4141@users.noreply.github.com>
2022-05-07 10:49:45 +0530

    Add files via upload

---
+++ src/Components/Home.js
@@ -0,0 +1,35 @@
+// import {ResponsiveContainer,LineChart,Line} from 'recharts';
+ 
+const data=[
+    {
+        name:"python",
+        student:13,
+        fees:10
+    },
+    {
+        name:"javascript",
+        student:15,
+        fees:12
+    },
+    {
+        name:"PHP",
+        student:5,
+        fees:10
+    },
+    {
+        name:"Java",
+        student:16,
+        fees:4
+    },
+    {
+        name:"C+",
+        student:14,
+        fees:8
+    },
+    {
+        name:"C++",
+        student:11,
+        fees:9
+    },
+
+]
